a47297faedebda72b583490c8742be7ad5307af9
Sandip Punalkar <sandippunalkar9@gmail.com>
2022-01-28 09:46:56 +0530

    Add the seed initial data

--- MovieList/Models/MovieContext.cs
+++ MovieList/Models/MovieContext.cs
@@ -9,5 +9,32 @@ namespace MovieList.Models
         }
 
         public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+              new Movie
+              {
+                  MovieId = 1,
+                  Name = "Casablanca",
+                  Year = 1942,
+                  Rating = 5
+              },
+              new Movie
+              {
+                  MovieId = 2,
+                  Name = "Wonder Woman",
+                  Year = 2017,
+                  Rating = 3
+              },
+              new Movie
+              {
+                  MovieId = 3,
+                  Name = "Moonstruck",
+                  Year = 1988,
+                  Rating = 4
+              }
+             );
+        }
     }
 }
